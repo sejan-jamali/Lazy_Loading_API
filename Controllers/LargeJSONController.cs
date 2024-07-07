c1fc1b709f9c9d1993b8ddd64db5f30a41a19f98
Sifat Haider Jamali <sifathaiderjamali@gmail.com>
2024-07-07 19:18:28 +0600

    Add files via upload

---
+++ Controllers/LargeJSONController.cs
@@ -0,0 +1,52 @@
+﻿using Lazy_Loading_API.Model;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Lazy_Loading_API.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class LargeJSONController : ControllerBase
+	{
+		private readonly ILogger<LargeJSON> _logger;
+		private readonly IHostEnvironment _hostEnvironment;
+
+		public LargeJSONController(ILogger<LargeJSON> logger, IHostEnvironment hostEnvironment)
+		{
+			_logger = logger;
+			_hostEnvironment = hostEnvironment;
+		}
+
+		[HttpGet("GetJsonFile")]
+		public string GetJSON()
+		{
+			var path = Path.Combine(_hostEnvironment.ContentRootPath, "DB", "largefile.json");
+			var jsonStr = System.IO.File.ReadAllText(path);
+			List<LargeJSON.Rootobject> items = JsonConvert.DeserializeObject<List<LargeJSON.Rootobject>>(jsonStr);
+			string limitedItem = JsonConvert.SerializeObject(items.Take(100));
+			return limitedItem;
+		}
+
+		[HttpGet("GetChunkData")]
+		public IActionResult GetChunkData(int start = 0, int count = 20)
+		{
+			var path = Path.Combine(_hostEnvironment.ContentRootPath, "DB", "largefile.json");
+			var jsonStr = System.IO.File.ReadAllText(path);
+			List<LargeJSON.Rootobject> items = JsonConvert.DeserializeObject<List<LargeJSON.Rootobject>>(jsonStr);
+
+			var paginatedItems = items.Skip(start).Take(count).ToList();
+
+			var totalCount = items.Count;
+			var response = new
+			{
+				Start = start,
+				Count = paginatedItems.Count,
+				Total = totalCount,
+				Items = paginatedItems
+			};
+
+			return Ok(response);
+		}
+	}
+}
+
---
+++ Model/LargeJSON.cs
@@ -0,0 +1,43 @@
+﻿namespace Lazy_Loading_API.Model
+{
+	public class LargeJSON
+	{
+
+		public class Rootobject
+		{
+			public string id { get; set; }
+			public string type { get; set; }
+			public Actor actor { get; set; }
+			public Repo repo { get; set; }
+			public Payload payload { get; set; }
+			public bool _public { get; set; }
+			public DateTime created_at { get; set; }
+		}
+
+		public class Actor
+		{
+			public int id { get; set; }
+			public string login { get; set; }
+			public string gravatar_id { get; set; }
+			public string url { get; set; }
+			public string avatar_url { get; set; }
+		}
+
+		public class Repo
+		{
+			public int id { get; set; }
+			public string name { get; set; }
+			public string url { get; set; }
+		}
+
+		public class Payload
+		{
+			public string _ref { get; set; }
+			public string ref_type { get; set; }
+			public string master_branch { get; set; }
+			public string description { get; set; }
+			public string pusher_type { get; set; }
+		}
+
+	}
+}
